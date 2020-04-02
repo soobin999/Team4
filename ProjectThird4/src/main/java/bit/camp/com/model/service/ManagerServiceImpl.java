ef0efd714f286ec64ec4213b51f7d64f436f73c8
fanahye <fanahye@gmail.com>
2020-04-02 18:12:58 +0900

    지혜

---
+++ ProjectThird4/src/main/java/bit/camp/com/model/service/ManagerServiceImpl.java
@@ -0,0 +1,25 @@
+package bit.camp.com.model.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import bit.camp.com.VO.mybatis.Reservation;
+import bit.camp.com.model.dao.ManagerDao;
+
+@Service("ManagerService")
+public class ManagerServiceImpl implements ManagerService{
+
+	@Override
+	public List<Reservation> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Reservation selectReservation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
